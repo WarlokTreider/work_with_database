d4d5ab63e0eaa5c81130283006aa6228a4a54820
WarlokTreider <163326255+WarlokTreider@users.noreply.github.com>
2024-09-09 21:29:22 +0300

    Update models.py

--- phones/models.py
+++ phones/models.py
@@ -3,13 +3,13 @@ from django.utils.text import slugify
 
 
 class Phone(models.Model):
-    id = models.AutoField(primary_key=True)  # Поле id, которое является основным ключом модели
-    name = models.CharField(max_length=255)  # Поле для названия телефона
-    price = models.DecimalField(max_digits=10, decimal_places=2)  # Поле для цены телефона
-    image = models.URLField(max_length=200, blank=True)  # Поле для ссылки на изображение телефона
-    release_date = models.DateField()  # Поле для даты выпуска телефона
-    lte_exists = models.BooleanField(default=False)  # Поле для флага наличия LTE
-    slug = models.SlugField(max_length=255, unique=True, blank=True)  # Поле slug
+    id = models.AutoField(primary_key=True)  
+    name = models.CharField(max_length=255)  
+    price = models.DecimalField(max_digits=10, decimal_places=2)  
+    image = models.URLField(max_length=200, blank=True)  
+    release_date = models.DateField()  
+    lte_exists = models.BooleanField(default=False)  
+    slug = models.SlugField(max_length=255, unique=True, blank=True)  
 
     def save(self, *args, **kwargs):
         if not self.slug:
